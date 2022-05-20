83beb9b83112f900892589ed1fcb9286f8f2fd13
ademuyiwa stephanie <stephanieademuyiwa1@gmail.com>
2022-05-20 20:04:58 +0100

    the addition of dropdown

--- app.py
+++ app.py
@@ -524,24 +524,49 @@ def shows():
     return render_template('pages/shows.html', shows=data)
 
 
-@app.route('/shows/create')
+@app.route('/shows/create', methods=['GET'])
 def create_shows():
     # renders form. do not touch.
     form = ShowForm()
-    return render_template('forms/new_show.html', form=form)
+    venue = Venue.query.all()
+    artist = Artist.query.all()
+    return render_template('forms/new_show.html', form=form, venues=venue, artists=artist)
 
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
     # called to create new shows in the db, upon submitting new show listing form
     # TODO: insert form data as a new Show record in the db, instead
-
+    form = ShowForm()
+    if form.validate_on_submit():
+      if Venue.query.get(request.form['venue_id']) and Artist.query.get(request.form['artist_id']):
+        new_show = Show(
+          venue_id=form.venue_id.data,
+          artist_id=form.artist_id.data,
+          start_time=form.start_time.data
+        )                
+        try:
+          db.session.add(new_show)
+          db.session.commit()
+          flash('Show was successfully listed!')
+        except:
+          db.session.rollback()
+          flash('Show was not successfully listed!')
+        finally:
+          db.session.close()
+      else:      
+        flash("An error occurred. the provided IDs don't exist, Show could not be listed.")
+        return render_template('forms/new_show.html')
+    else:
+      for error_message in form.errors.values():
+            flash(f'An error occurred on {error_message[0]}, Show ' +
+                  request.form['name'] + ' could not be listed.')
+    return render_template('pages/home.html')
     # on successful db insert, flash success
-    flash('Show was successfully listed!')
+    
     # TODO: on unsuccessful db insert, flash an error instead.
     # e.g., flash('An error occurred. Show could not be listed.')
     # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-    return render_template('pages/home.html')
 
 
 @app.errorhandler(404)

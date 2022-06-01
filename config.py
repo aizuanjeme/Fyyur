124fceb4db6772e28ca5340fe9b8a80a0bf774a0
ademuyiwa stephanie <stephanieademuyiwa1@gmail.com>
2022-06-01 16:20:20 +0100

    the correction of the database

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 SECRET_KEY = os.urandom(32)
 # SECRET_KEY = os.getenv('SECRET_KEY')
 
@@ -17,6 +20,9 @@ WTF_CSRF_ENABLED = False
 
 # TODO IMPLEMENT DATABASE URL
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:!pass4sure@localhost:5432/fyyur'
-SQLALCHEMY_DATABASE_URI = 'postgresql://cedulktzyrtvbp:d90034e6b7ebdadf141847868625f745cd9557d6e2f6d3ce8c912874bfb9ecfc@ec2-34-227-120-79.compute-1.amazonaws.com:5432/d8tncga3uf15s2'
-# SQLALCHEMY_DATABASE_URI = 'postgres://cedulktzyrtvbp:d90034e6b7ebdadf141847868625f745cd9557d6e2f6d3ce8c912874bfb9ecfc@ec2-34-227-120-79.compute-1.amazonaws.com:5432/d8tncga3uf15s2'
+# When deploying to Heroku
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)

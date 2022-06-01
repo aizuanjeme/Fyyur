da7975e08cd80e54e54febc0ffed2564a29dea5b
ademuyiwa stephanie <stephanieademuyiwa1@gmail.com>
2022-06-01 19:03:58 +0100

    the clean up

--- config.py
+++ config.py
@@ -19,10 +19,9 @@ WTF_CSRF_ENABLED = False
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:!pass4sure@localhost:5432/fyyur'
 # When deploying to Heroku
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
-# if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-#     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
